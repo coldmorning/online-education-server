858c50bcbf8d22bd5f5f81871582f048e3ce1260
Huang coldmorning <coldmorning360@gmail.com>
2021-09-23 15:43:33 +0800

    Fix bug(auth): Fix the description for a ResultCode  file.

--- src/main/java/com/coldmorning/online/education/auth/util/response/ResultCode.java
+++ src/main/java/com/coldmorning/online/education/auth/util/response/ResultCode.java
@@ -11,9 +11,9 @@ public enum ResultCode {
     SUCESS_Login(ResultHttpStatus.status200,0000,"登入成功"),
     ERROR_Login(ResultHttpStatus.status500,1001,"登入失敗"),
 
-    SUCESS_Register(ResultHttpStatus.status500,0010,"註冊成功"),
+    SUCESS_Register(ResultHttpStatus.status200,0010,"註冊成功"),
     ERROR_Register_Exist(ResultHttpStatus.status500,1011,"註冊失敗, 帳號已存在"),
-    ERROR_Register_RePassword(ResultHttpStatus.status500,1012,"註冊成功, 兩次輸入密碼不同"),
+    ERROR_Register_RePassword(ResultHttpStatus.status500,1012,"註冊失敗, 兩次輸入密碼不同"),
     ERROR_Register_Other(ResultHttpStatus.status500,1013,"註冊失敗, 其他錯誤，請洽詢服務人員");
 
 
