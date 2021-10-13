aa03131e1ac0b0f4428811082852d51e1e924abf
Huang coldmorning <coldmorning360@gmail.com>
2021-10-13 18:53:47 +0800

    fix(Auth: util): Update  the rule of http status code to auth feature.

--- src/main/java/com/coldmorning/online/education/auth/util/response/ResultCode.java
+++ src/main/java/com/coldmorning/online/education/auth/util/response/ResultCode.java
@@ -9,11 +9,11 @@ public enum ResultCode {
     ERROR(ResultHttpStatus.status500,1000,"伺服器處理異常"),
 
     SUCESS_Login(ResultHttpStatus.status200,0000,"登入成功"),
-    ERROR_Login(ResultHttpStatus.status500,1001,"登入失敗"),
+    ERROR_Login(ResultHttpStatus.status401,1001,"登入失敗"),
 
-    SUCESS_Register(ResultHttpStatus.status200,0010,"註冊成功"),
-    ERROR_Register_Exist(ResultHttpStatus.status500,1011,"註冊失敗, 帳號已存在"),
-    ERROR_Register_RePassword(ResultHttpStatus.status500,1012,"註冊失敗, 兩次輸入密碼不同"),
+    SUCESS_Register(ResultHttpStatus.status201,0010,"註冊成功"),
+    ERROR_Register_Exist(ResultHttpStatus.status422,1011,"註冊失敗, 帳號已存在"),
+    ERROR_Register_RePassword(ResultHttpStatus.status400,1012,"註冊失敗, 兩次輸入密碼不同"),
     ERROR_Register_Other(ResultHttpStatus.status500,1013,"註冊失敗, 其他錯誤，請洽詢服務人員");
 
 
--- src/main/java/com/coldmorning/online/education/auth/util/response/ResultHttpStatus.java
+++ src/main/java/com/coldmorning/online/education/auth/util/response/ResultHttpStatus.java
@@ -4,5 +4,9 @@ import org.springframework.http.HttpStatus;
 
 public class ResultHttpStatus {
     public static final HttpStatus status200 = HttpStatus.OK;
+    public static final HttpStatus status201 = HttpStatus.CREATED;
+    public static final HttpStatus status400 = HttpStatus.BAD_REQUEST;
+    public static final HttpStatus status401 = HttpStatus.UNAUTHORIZED;
+    public static final HttpStatus status422 = HttpStatus.UNPROCESSABLE_ENTITY;
     public static final HttpStatus status500 = HttpStatus.INTERNAL_SERVER_ERROR;
 }
